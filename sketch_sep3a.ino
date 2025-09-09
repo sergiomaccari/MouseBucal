d1692eaca36b90fc919a4e5430a71563db715a9b
Sergio Roncato Maccari <sergio.r.maccari@gmail.com>
2025-09-09 14:53:10 -0300

    Update sketch_sep3a.ino
    Código atualizado para Arduino Leonardo

--- sketch_sep3a.ino
+++ sketch_sep3a.ino
@@ -1,16 +1,20 @@
+#include <Mouse.h>
 
-#define UP 12   // Cima
-#define DWN 11  // Baixo
-#define LFT 10  // Esquerda
-#define RHT 9   // Direita
-#define MID 8   // Centro
-#define SET 7   // Set
-#define RST 6   // Reset
-#define BTN1 5  // Botão 1
-#define BTN2 4 //Botao 2
+// Definições dos pinos
+#define UP   12  // Cima
+#define DWN  11  // Baixo
+#define LFT  10  // Esquerda
+#define RHT   9  // Direita
+#define MID   8  // Centro (clique do meio)
+#define SET   7  // Botão extra Set
+#define RST   6  // Botão extra Reset
+#define BTN1  5  // Botão 1 -> Botão direito do mouse
+#define BTN2  4  // Botão 2 -> Botão esquerdo do mouse
 
 void setup() {
   Serial.begin(9600);
+
+  // Configura os pinos como entradas com pull-up interno
   pinMode(UP, INPUT_PULLUP);
   pinMode(DWN, INPUT_PULLUP);
   pinMode(LFT, INPUT_PULLUP);
@@ -18,58 +22,63 @@ void setup() {
   pinMode(MID, INPUT_PULLUP);
   pinMode(SET, INPUT_PULLUP);
   pinMode(RST, INPUT_PULLUP);
-  pinMode(BTN1, INPUT_PULLUP);  // configura botão 1
+  pinMode(BTN1, INPUT_PULLUP);
   pinMode(BTN2, INPUT_PULLUP);
+
+  // Inicializa o controle do mouse
+  Mouse.begin();
 }
 
-void loop() { //equivalente a main
+void loop() {
   lerJoystick();
-  lerBotao1();
-  lerBotao2();
+  lerBotoes();
 }
 
-void lerJoystick() { //le o joystick
-  if(!digitalRead(UP)) {
-    Serial.println("Cima");
+// Movimento do joystick
+void lerJoystick() {
+  if (!digitalRead(UP)) {
+    Mouse.move(0, -10); // Move para cima
+    delay(25);
+  }
+  if (!digitalRead(DWN)) {
+    Mouse.move(0, 10);  // Move para baixo
+    delay(25);
+  }
+  if (!digitalRead(LFT)) {
+    Mouse.move(-10, 0); // Move para esquerda
+    delay(25);
+  }
+  if (!digitalRead(RHT)) {
+    Mouse.move(10, 0);  // Move para direita
+    delay(25);
+  }
+  if (!digitalRead(MID)) {
+    Mouse.click(MOUSE_MIDDLE); // Clique botão do meio
     delay(200);
   }
-  if(!digitalRead(DWN)) {
-    Serial.println("Baixo");
+  if (!digitalRead(SET)) {
+    Serial.println("Set pressionado");
     delay(200);
   }
-  if(!digitalRead(LFT)) {
-    Serial.println("Esquerda");
-    delay(200);
-  }
-  if(!digitalRead(RHT)) {
-    Serial.println("Direita");
-    delay(200);
-  }
-  if(!digitalRead(MID)) {
-    Serial.println("Centro");
-    delay(200);
-  }
-  if(!digitalRead(SET)) {
-    Serial.println("Set");
-    delay(200);
-  }
-  if(!digitalRead(RST)) {
-    Serial.println("Reset");
-    delay(200);
-  }
-}
-
-void lerBotao1() { //nome autoexplicativo
-  if(!digitalRead(BTN1)) {
-    Serial.println("Botão 1");
+  if (!digitalRead(RST)) {
+    Serial.println("Reset pressionado");
     delay(200);
   }
 }
 
-void lerBotao2() { //nome autoexplicativo
-  if(!digitalRead(BTN2)) {
-    Serial.println("Botão 2");
-    delay(200);
+// Clique dos botões extras
+void lerBotoes() {
+  if (!digitalRead(BTN1)) {
+    Mouse.press(MOUSE_RIGHT);   // Pressiona botão direito
+    delay(50);
+  } else {
+    Mouse.release(MOUSE_RIGHT); // Solta botão direito
+  }
+
+  if (!digitalRead(BTN2)) {
+    Mouse.press(MOUSE_LEFT);    // Pressiona botão esquerdo
+    delay(50);
+  } else {
+    Mouse.release(MOUSE_LEFT);  // Solta botão esquerdo
   }
 }
-
